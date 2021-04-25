4a43e013923f66d227b7f11958cbb1542948629b
kareinonitsuke <>
2021-04-25 17:39:35 +0900

    [fix] Discoed周り修正

--- discord.js
+++ discord.js
@@ -1,7 +1,7 @@
 function post_discord(message){
-    const url        = 'https://discordapp.com/api/webhooks/835343084056346664/axpWPVwAmPEe5ciSMSYbCtZFhA5z7g0z1sbFd-0e68bK7ndcnUjKjWQDJqYb0S8e_O9b';
-    const token      = 'axpWPVwAmPEe5ciSMSYbCtZFhA5z7g0z1sbFd-0e68bK7ndcnUjKjWQDJqYb0S8e_O9b';
-    const channel    = '#定例会アナウンス';
+    const url        = PropertiesService.getScriptProperties().getProperty("DISCORDURL");
+    const token      = PropertiesService.getScriptProperties().getProperty("DISCORDTOKEN");
+    const channel    = PropertiesService.getScriptProperties().getProperty("DISCORDCHANNEL");
     const text       = message;
     const username   = 'スケジュール調整システム(試運転)';
     const parse      = 'full';
@@ -24,6 +24,6 @@ function post_discord(message){
    response = UrlFetchApp.fetch(url, params);
 }
 
-function test(){
+function discord_test(){
     post_discord("投稿テストです");
 }
--- mail.js
+++ mail.js
@@ -3,7 +3,7 @@ function send_email(subject, text){
     const recipient = PropertiesService.getScriptProperties().getProperty("RECIPIENT");
     const options = {name: 'スケジュール調整システム'};
     const recipientname = PropertiesService.getScriptProperties().getProperty("RECIPIENTNAME");
-    const body = `${recipientname}\n\n${text}`;
+    const body = `${recipientname}\n定例会日程調整システム(試運転中)です。\n\n${text}`;
   
     GmailApp.sendEmail(recipient, subject, body, options);
   }
