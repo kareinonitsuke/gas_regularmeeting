c09a79b4129985cb21e11f792941c175d5dc1d4f
kareinonitsuke <>
2021-10-11 22:59:35 +0900

    [fix]バグ修正とリファクタリング バグ修正箇所 ・.Dateとすべきところ.Dayになっている

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ function set_val(){
   PropertiesService.getScriptProperties().setProperty("SURVEYFORMID", "");
   PropertiesService.getScriptProperties().setProperty("SURVEYFORMURL","");
   PropertiesService.getScriptProperties().setProperty("DISCORDMENTION","");
-  PropertiesService.getScriptProperties().setProperty("ANNOUNCETXT","")
+  PropertiesService.getScriptProperties().setProperty("REMINDERANNOUNCETXT","")
+  PropertiesService.getScriptProperties().setProperty("REMINDERSUBJECT","")
   //スクリプトプロパティを取得する
   Logger.log(PropertiesService.getScriptProperties().getProperty("RECIPIENT"));
   Logger.log(PropertiesService.getScriptProperties().getProperty("RECIPIENTNAME"));
@@ -26,7 +27,8 @@ function set_val(){
   Logger.log(PropertiesService.getScriptProperties().getProperty("SURVEYFORMID"));
   Logger.log(PropertiesService.getScriptProperties().getProperty("SURVEYFORMURL"));
   Logger.log(PropertiesService.getScriptProperties().getProperty("DISCORDMENTION"));
-  Logger.log(PropertiesService.getScriptProperties().getProperty("ANNOUNCETXT"));
+  Logger.log(PropertiesService.getScriptProperties().getProperty("REMINDERANNOUNCETXT"));
+  Logger.log(PropertiesService.getScriptProperties().getProperty("REMINDERSUBJECT"));
 }
 
 //集計開始日の設定
@@ -40,22 +42,34 @@ function set_survaydate(){
 }
 
 //offset日後の日時を取得する関数                                                
-function get_date(offset){
+function get_date(offset_date, based_on_today_date, offset_month, based_on_today_month){
   const date = new Date();//const?
-  date.setDate(date.getDate() + offset);
+  if(based_on_today_month==1){
+    date.setMonth(date.getMonth() + offset_month);
+  }
+  else{
+    date.setMonth(offset_month);
+  }
+
+  if(based_on_today_date==1){
+    date.setDate(date.getDate() + offset_date);
+  }
+  else{
+    date.setDate(offset_date);
+  }
   return date;
 }
 
 //offset日後の成形された日時を取得する関数
-function get_datestr(offset){
-  const date = get_date(offset);
+function get_datestr(offset_date, based_on_today_date, offset_month, based_on_today_month){
+  const date = get_date(offset_date, based_on_today_date, offset_month, based_on_today_month);
   const datestr = Utilities.formatDate(date, "JST", "MM/dd");
   return datestr;
 }
 
 //funcnameをoffset日後実行するようTriggerを設定する関数
-function set_trigger(funcname, offset){
-  const date = get_date(offset);
+function set_trigger(funcname, offset_date, based_on_today_date, offset_month, based_on_today_month){
+  const date = get_date(offset_date, based_on_today_date, offset_month, based_on_today_month);
   ScriptApp.newTrigger(funcname).timeBased().at(date).create();
 }
 
@@ -68,11 +82,11 @@ function request_answer(){
   const simekiri_offset = 6;
   
   //集計開始日と締め切り日を取得
-  const today = get_datestr(0);
-  const simekiri = get_datestr(simekiri_offset);
+  const today = get_datestr(0, 1, 0, 1);
+  const simekiri = get_datestr(simekiri_offset, 1, 0, 1);
 
   //結果集計のトリガーを設定
-  set_trigger('announce_result', simekiri_offset+1);
+  set_trigger('announce_result', simekiri_offset+1, 1, 0, 1);
   
   //メール本文
   const subject = PropertiesService.getScriptProperties().getProperty("REQUESTANSWERSUBJECT");
@@ -93,10 +107,11 @@ function announce_result(){
   let text = PropertiesService.getScriptProperties().getProperty("ANNOUNCERESULTTEXT");
   const date = getResultSchedule();
 
-  let kouhobi = new Date();
-  kouhobi.setMonth(kouhobi.getMonth() + 1);
-  kouhobi.setDate(date+1);
-  let datestr = Utilities.formatDate(kouhobi, "JST", "MM/dd");
+  let kouhobi = get_date(date+1, 0, 1, 1);
+  // kouhobi.setMonth(kouhobi.getMonth() + 1);
+  // kouhobi.setDate(date+1);
+  // let datestr = Utilities.formatDate(kouhobi, "JST", "MM/dd");
+  let datestr =get_datestr(kouhobi.getDate(), 0, kouhobi.getMonth(), 0);
 
   let arrDay = new Array('日', '月', '火', '水', '木', '金', '土');
   let youbi = arrDay[kouhobi.getDay()];
@@ -110,13 +125,15 @@ function announce_result(){
   //本文を指定してDiscoedに投稿
   post_discord(text);
 
-  PropertiesService.getScriptProperties().setProperty("ANNOUNCETXT",`今月の定例会は${datestr}(${youbi})です。\n`)
-  ScriptApp.newTrigger("announce_reminder_bf2").timeBased().at(kouhobi).create();
+  PropertiesService.getScriptProperties().setProperty("REMINDERANNOUNCETXT",`${datestr}(${youbi})です。\n`)
+  // ScriptApp.newTrigger("announce_reminder_bf0").timeBased().at(kouhobi).create();
+  set_trigger("announce_reminder_bf0", kouhobi.getDate(), 0, kouhobi.getMonth(), 0);
 
-  let kouhobi_3before = new Date();
-  kouhobi_3before.setMonth(kouhobi.getMonth());
-  kouhobi_3before.setDate(kouhobi.getDay()+3);
-  ScriptApp.newTrigger("announce_reminder_bf0").timeBased().at(kouhobi_3before).create();
+  // let kouhobi_3before = new Date();
+  // kouhobi_3before.setMonth(kouhobi.getMonth());
+  // kouhobi_3before.setDate(kouhobi.getDay()+3);
+  set_trigger("announce_reminder_bf3", kouhobi.getDate()-3, 0, kouhobi.getMonth(), 0);
+  //ScriptApp.newTrigger("announce_reminder_bf3").timeBased().at(kouhobi_3before).create();
 
   //件名と本文を指定してメール送信
   send_email(subject, text);
@@ -124,9 +141,9 @@ function announce_result(){
   post_discord(text);
 }
 
-function announce_reminder_bf2(){
-  const subject = PropertiesService.getScriptProperties().getProperty("ANNOUNCERESULTSUBJECT");
-  const text = PropertiesService.getScriptProperties().getProperty("ANNOUNCETXT");
+function announce_reminder_bf3(){
+  const subject = PropertiesService.getScriptProperties().getProperty("REMINDERSUBJECT");
+  const text = "今月の定例会は3日後の"+PropertiesService.getScriptProperties().getProperty("REMINDERANNOUNCETXT");
   //件名と本文を指定してメール送信
   send_email(subject, text);
   //本文を指定してDiscoedに投稿
@@ -134,8 +151,8 @@ function announce_reminder_bf2(){
 }
 
 function announce_reminder_bf0(){
-  const subject = PropertiesService.getScriptProperties().getProperty("ANNOUNCERESULTSUBJECT");
-  const text = PropertiesService.getScriptProperties().getProperty("ANNOUNCETXT");
+  const subject = PropertiesService.getScriptProperties().getProperty("REMINDERSUBJECT");
+  const text = "今日は定例会当日の"+PropertiesService.getScriptProperties().getProperty("REMINDERANNOUNCETXT");
   //件名と本文を指定してメール送信
   send_email(subject, text);
   //本文を指定してDiscoedに投稿
